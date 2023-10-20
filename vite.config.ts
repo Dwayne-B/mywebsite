87eb951470d1c62fd9b4be93f8257366be34d7be
Dwayne Bynum <dbynum4597@gmail.com>
2023-10-20 00:33:16 -0400

    error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.', // Specify the correct path to your project's root directory
   plugins: [react()],
-})
+});
