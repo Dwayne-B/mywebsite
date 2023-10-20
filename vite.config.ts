17879ee35490756c93256598b34779d96dee3ae0
Dwayne Bynum <dbynum4597@gmail.com>
2023-10-20 00:36:39 -0400

    ??

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.', // Specify the correct path to your project's root directory
+  root: './', // Specify the correct path to your project's root directory
   plugins: [react()],
 });
