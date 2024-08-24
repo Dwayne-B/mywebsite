689432b2c87e65a5b38efa26a8f808412b1b7f0b
devlab333 <dwaynebynum5@gmail.com>
2024-08-24 04:17:55 -0400

    :zap:

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import{useState} from 'react'
-import './App.scss'
-import Nav from './components/Nav/Nav'
-import Hero from './sections/Hero/Hero'
-import About from './sections/About/About'
-import Projects from './sections/Projects/Projects'
-import Contact from './sections/Contact/Contact'
-import Footer from './sections/Footer/Footer'
+import { useState } from "react";
+import "./App.scss";
+import Nav from "./components/Nav/Nav";
+import Hero from "./sections/Hero/Hero";
+import About from "./sections/About/About";
+import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact/Contact";
+import Footer from "./sections/Footer/Footer";
 
 function App() {
-  const [modalIsOpen,setModalIsOpen] =useState(false);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  return (
-   <div className='text-white app-container '>
-    <Nav modalIsOpen={modalIsOpen}/>
-    <Hero/>
-    <About/>
-    <Projects modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}/>
-    <Contact/>
-    <Footer/>
-   </div>
-  )
+	return (
+		<div className='text-white app-container '>
+			<Nav modalIsOpen={modalIsOpen} />
+			<Hero />
+			<About />
+			<Projects
+				modalIsOpen={modalIsOpen}
+				setModalIsOpen={setModalIsOpen}
+			/>
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
