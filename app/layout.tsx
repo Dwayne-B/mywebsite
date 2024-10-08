e6a8b1541bfdcece68a86cbb29aa319ec650ac8f
= <=>
2024-10-08 19:59:08 -0400

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,24 @@ import type { Metadata } from "next";
 import "./globals.scss";
 
 export const metadata: Metadata = {
-  title: "Dwayne Bynum",
-  description: "portfolio to showcase my skills as a developer!",
+	title: "Dwayne Bynum",
+	description: "portfolio to showcase my skills as a developer!",
+	icons: {
+		icon: "/favicon-32x32.png",
+		apple: "/apple-touch-icon.png",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en'>
-      <body className=''>
-        <div className='wrapper'>{children}</div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className=''>
+				<div className='wrapper'>{children}</div>
+			</body>
+		</html>
+	);
 }
